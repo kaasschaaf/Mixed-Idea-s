73126bcfa73deb9279ed9bbb8bf81401d2399211
kaasschaaf <jesse.dj@hotmail.com>
2013-10-27 00:02:13 +0200

    Delete Reference.java

--- M_I common/mixed_ideas/lib/Reference.java
+++
@@ -1,7 +0,0 @@
-package mixed_ideas.lib;
-
-public class Reference {
-    public static final String MOD_ID = "MixedIdeas";
-    public static final String MOD_NAME = "Mixed ideas";
-    public static final String MOD_VERSION = "0.0.1";
-}
